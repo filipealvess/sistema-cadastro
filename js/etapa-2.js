fab0965d6cb4cc42e3419d8ec10c4eecd7e7451e
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-11 09:54:15 -0300

    Save segments in localStorage

--- js/etapa-2.js
+++ js/etapa-2.js
@@ -1,7 +1,35 @@
 const cards = document.querySelectorAll('.card-hover');
+const anotherSegmentInput = document.querySelector('[name="another-segment"]');
+
+function saveData(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
 
 function toggleCardSelection(card) {
   card.classList.toggle('active-card');
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const cardList = Array.from(cards);
+  const activeCards = cardList.filter(card => card.classList.contains('active-card'));
+  const anotherSegment = anotherSegmentInput.value;
+  const nothingSelected = activeCards.length === 0 && anotherSegment.length === 0;
+
+  if (nothingSelected) {
+    anotherSegmentInput.focus();
+    return;
+  }
+
+  const segments = activeCards.map(card => {
+    const segment = card.querySelector('p').innerText;
+    return segment;
+  });
+
+  anotherSegment && segments.push(anotherSegment);
+  saveData('segments', segments);
+  window.location.replace('/etapa-3.html');
 }
 
 cards.forEach(card => {
