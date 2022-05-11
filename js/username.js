c755eefc5c483423cfd1135200d0654f6bc538ba
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-11 10:22:29 -0300

    Get saved user name

---
+++ js/username.js
@@ -0,0 +1,19 @@
+const userName = document.querySelector('#user-name');
+
+function navigateTo(path) {
+  window.location.replace(path);
+}
+
+function updateUserName() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (!user) {
+    navigateTo('/');
+  }
+
+  if (userName) {
+    userName.innerText = user.name;
+  }
+}
+
+updateUserName();
